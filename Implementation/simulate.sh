9ddd46fde79e2c7cd310fd6c1869f256a4ceef23
Henry Lovett <hl13g10@ecs.soton.ac.uk>
2014-02-18 23:08:15 +0000

    Renamed the testbenches.
    Using simulation file from VLSI

--- Implementation/simulate.sh
+++ Implementation/simulate.sh
@@ -1,14 +1,80 @@
-#!/bin/bash
-# @file simulate.sh
-# Date Created: Mon 17 Feb 2014 09:36:09 GMT by seblovett on hind.ecs.soton.ac.uk
-# <+Last Edited: Mon 17 Feb 2014 11:09:32 GMT by hl13g10 on hind.ecs.soton.ac.uk +>
-
-echo ELEC6016 Simulation
+#! /bin/bash
 
 
+# HELP function
+function printHelp (){
+   echo "-------- HELP --------"
+   echo "Parameter 1: Module"
+   ls | grep _stim.sv
+   echo "N.B. run without '_stim.sv' after name"
+#   echo "   alu"
+#   echo "   control"
+#   echo "   cpu_core"
+#   echo "   datapath"
+#   echo "   regBlock"
+   echo "Paramter 2: GUI"
+   echo "   no_gui  "
+   echo "   gui      (default)"
+}
 
-ncverilog -sv -q -y . +libext+.sv opcodes.svh $1_tb.sv
- 
 
-exit 0
+# GUI Option
+cmd="ncverilog -sv"
+gui="$2"
+gui=${gui//[-.]/} # Remove tag
+case $gui in
+   "no_gui")      echo "No GUI";;
+   *)             echo "GUI" ; cmd="$cmd +gui" ;; 
+esac
+cmd="$cmd +ncaccess+r"
 
+
+# Which module to test
+module="$1"
+module=${module//[-.]/} # Remove tag
+#case $module in
+#   "alu")         cmd="$cmd   opcodes.svh    
+#                              alu_stim.sv       
+#                              alu.sv"           ;;
+#   "control")     cmd="$cmd   opcodes.svh    
+#                              control_stim.sv   
+#                              control.sv"       ;;
+#   "cpu_core")    cmd="$cmd   opcodes.svh    
+#                              cpu_core_stim.sv  
+#                              cpu_core.sv    
+#                              control.sv  
+#                              datapath.sv    
+#                              alu.sv
+#                              regBlock.sv
+#                              trisBuf16.sv
+#                              signExtend.sv"       ;;
+#   "datapath")    cmd="$cmd   opcodes.svh        
+#                              datapath_stim.sv  
+#                              datapath.sv       
+#                              alu.sv            
+#                              regBlock.sv       
+#                              trisBuf16.sv     ";;
+#   "regBlock")    cmd="$cmd                  regBlock_stim.sv  regBlock.sv"                                       ;;
+#   "trisreg")	  cmd="$cmd trisreg_stim.sv trisreg.sv";;
+#   "regBlock")    cmd="$cmd   regBlock_stim.sv  
+#                              regBlock.sv"      ;;
+#   *)             echo "Unkown module $module"; printHelp; exit ;;
+#esac
+#HSL: much simpler
+if [ -f ${module}_stim.sv ]; then
+	cmd="$cmd +libext+.sv -y . ${module}_stim.sv opcodes.svh"
+else
+	echo "Unkown module $module"
+	printHelp;
+	exit 1;
+fi
+#cmd="$cmd +libext+.sv"
+
+echo $cmd
+$cmd  # Call SimVision
+
+
+# I am sure there's a point for these but...
+# HSL: if this line is included, you can not reinvoke simulator. Maybe best to
+# have a clean script?
+#rm -rf ncverilog.log ncverilog.key waves.shm INCA_libs
