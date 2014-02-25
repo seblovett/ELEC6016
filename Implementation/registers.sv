51a553438c28967424ebfe037c44041e9cbb205b
seblovett <seblovett@googlemail.com>
2014-02-25 11:55:06 +0000

    changed to one port for read / write

--- Implementation/registers.sv
+++ Implementation/registers.sv
@@ -1,14 +1,14 @@
 // registers.sv
 // Writen by seblovett
 // Date Created Mon 17 Feb 2014 10:16:30 GMT
-// <+Last Edited: Mon 17 Feb 2014 13:16:02 GMT by hl13g10 on hind.ecs.soton.ac.uk +>
+// <+Last Edited: Tue 25 Feb 2014 11:39:06 GMT by hl13g10 on hind.ecs.soton.ac.uk +>
 
 
 module registers #(parameter n = 8, parameter addr_width = 5, parameter regcount = 10) ( //n - data bus width, addr_width - width of the address bus, regcount - number of registers
-	input logic Clock, Reset, WE,  //control signals
-	output logic [n-1:0] Rd1, Rd2, //output data
-	input logic [addr_width - 1 :0] Rs1, Rs2, Rw,//input addresses
-	input logic [n-1:0] Data //input data
+	input wire  Clock, Reset, WE,  //control signals
+	output logic [n-1:0] Rd1, //output data
+	input wire  [addr_width - 1 :0] Rs1,//input addresses
+	input wire  [n-1:0] Data //input data
 );
 
 timeunit 1ns; timeprecision 1ps;
@@ -22,11 +22,11 @@ begin
 	else
 	 begin
 		if (WE)
-			regs[Rw] <= Data;
+			regs[Rs1] <= Data;
 	 end
 end
 
 assign Rd1 = regs[Rs1];
-assign Rd2 = regs[Rs2];
+//assign Rd2 = regs[Rs2];
 
 endmodule
