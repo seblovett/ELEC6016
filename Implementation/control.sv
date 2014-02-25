40b456441e40f0296a8df7b9d238d390da6f71d9
seblovett <seblovett@googlemail.com>
2014-02-25 12:28:15 +0000

    Started implementing STSW

--- Implementation/control.sv
+++ Implementation/control.sv
@@ -1,7 +1,7 @@
 // control.sv
 // Writen by seblovett
 // Date Created Tue 18 Feb 2014 23:21:44 GMT
-// <+Last Edited: Tue 25 Feb 2014 11:51:17 GMT by hl13g10 on hind.ecs.soton.ac.uk +>
+// <+Last Edited: Tue 25 Feb 2014 12:18:39 GMT by hl13g10 on hind.ecs.soton.ac.uk +>
 
 
 module control (
@@ -16,9 +16,29 @@ timeunit 1ns; timeprecision 1ps;
 
 import opcodes::*;
 
-assign AluOp = ALU_ADD;
-assign RegWe = 0;
-assign WDataSel = 0;
-assign PcWait = 0;
+
+always_comb
+begin
+	//some defaults
+	RegWe = 0;
+	WDataSel = 0;
+	PcWait = 0;
+	AluOp = ALU_NOOP;
+	
+	case(OpCode)
+	//NOOP  :	//Use defaults
+//	WAIT  :	
+	STSW  :	begin
+			WDataSel = 1; //Choose switches
+			RegWe    = 1; //Write to Reg
+		end
+//	LEDS  :	
+//	PASSA :	
+//	ADD   :	
+//	MULT  :	
+//	STACC :	
+	endcase
+
+end
 endmodule
 
