9901e8f5d8e01efa06d68d4ce65f01da40eee3d0
seblovett <seblovett@googlemail.com>
2014-02-25 12:26:55 +0000

    now runs tcl file if exists

--- Implementation/simulate.sh
+++ Implementation/simulate.sh
@@ -22,46 +22,22 @@ function printHelp (){
 cmd="ncverilog -sv"
 gui="$2"
 gui=${gui//[-.]/} # Remove tag
-case $gui in
-   "no_gui")      echo "No GUI";;
-   *)             echo "GUI" ; cmd="$cmd +gui" ;; 
-esac
-cmd="$cmd +ncaccess+r"
 
 
 # Which module to test
 module="$1"
 module=${module//[-.]/} # Remove tag
-#case $module in
-#   "alu")         cmd="$cmd   opcodes.svh    
-#                              alu_stim.sv       
-#                              alu.sv"           ;;
-#   "control")     cmd="$cmd   opcodes.svh    
-#                              control_stim.sv   
-#                              control.sv"       ;;
-#   "cpu_core")    cmd="$cmd   opcodes.svh    
-#                              cpu_core_stim.sv  
-#                              cpu_core.sv    
-#                              control.sv  
-#                              datapath.sv    
-#                              alu.sv
-#                              regBlock.sv
-#                              trisBuf16.sv
-#                              signExtend.sv"       ;;
-#   "datapath")    cmd="$cmd   opcodes.svh        
-#                              datapath_stim.sv  
-#                              datapath.sv       
-#                              alu.sv            
-#                              regBlock.sv       
-#                              trisBuf16.sv     ";;
-#   "regBlock")    cmd="$cmd                  regBlock_stim.sv  regBlock.sv"                                       ;;
-#   "trisreg")	  cmd="$cmd trisreg_stim.sv trisreg.sv";;
-#   "regBlock")    cmd="$cmd   regBlock_stim.sv  
-#                              regBlock.sv"      ;;
-#   *)             echo "Unkown module $module"; printHelp; exit ;;
-#esac
+
+
+if [ "gui" == $gui ]; then
+	
+	cmd="$cmd +gui +ncaccess+r"
+	if [ -f "${module}.tcl" ]; then
+		cmd="$cmd +tcl+${module}.tcl"
+	fi
+fi
 #HSL: much simpler
-if [ -f ${module}_stim.sv ]; then
+if [ -f "${module}_stim.sv" ]; then
 	cmd="$cmd +libext+.sv -y . ${module}_stim.sv opcodes.svh"
 else
 	echo "Unkown module $module"
