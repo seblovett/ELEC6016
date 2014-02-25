a16023bbf45b2a3220361f1e801b3311d4621562
seblovett <seblovett@googlemail.com>
2014-02-25 12:52:31 +0000

    Error with the port sizes

--- Implementation/alu.sv
+++ Implementation/alu.sv
@@ -1,18 +1,21 @@
 // alu.sv
 // Writen by seblovett
 // Date Created Mon 17 Feb 2014 09:18:54 GMT
-// <+Last Edited: Tue 25 Feb 2014 11:54:00 GMT by hl13g10 on hind.ecs.soton.ac.uk +>
+// <+Last Edited: Tue 25 Feb 2014 12:51:05 GMT by hl13g10 on hind.ecs.soton.ac.uk +>
 
 
 module alu #(parameter n = 8) //n - data bus width
 (
-	input logic [n:0] a, b, 
+	input logic [n-1:0] a, b, 
 	input opcodes::alu_functions_t Function,
-	output logic [n:0] q);
+	output logic [n-1:0] q);
 
 timeunit 1ns; timeprecision 1ps;
 
 import opcodes::*;
+
+logic [2*n - 1:0] multout;
+assign multout = a*b;
 
 always_comb
  begin
@@ -20,7 +23,7 @@ always_comb
 	unique case (Function)
 		ALU_A:		q = a;
 		ALU_ADD:	q = a + b;
-		ALU_MULT:	q = a * b;
+		ALU_MULT:	q = multout[n-1:0];
 		default:	q = 0;
 	endcase
 	
