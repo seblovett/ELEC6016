8b3bb4104d5629cee2308f9b8dc69929b3418909
seblovett <seblovett@googlemail.com>
2014-03-26 13:04:16 +0000

    Put one set of data in the test

--- Implementation/cpu_stim.sv
+++ Implementation/cpu_stim.sv
@@ -1,7 +1,7 @@
 // cpu_stim.sv
 // Writen by seblovett
 // Date Created Tue 18 Feb 2014 23:23:59 GMT
-// <+Last Edited: Tue 25 Mar 2014 12:49:20 GMT by hl13g10 on hind.ecs.soton.ac.uk +>
+// <+Last Edited: Wed 26 Mar 2014 12:44:59 GMT by hl13g10 on hind.ecs.soton.ac.uk +>
 
 
 module cpu_stim ();
@@ -12,7 +12,7 @@ timeunit 1ns; timeprecision 1ps;
 parameter n = 8;
 logic Clock, nReset;
 logic [9:0] Switches;
-wire [n-1: 0] LEDs;
+wire [7: 0] LEDs;
 
 cpu #(.n(n)) c (.*);
 //clock
@@ -41,8 +41,8 @@ begin
 end
 initial
 begin
-	#60000 Switches[8] = 1; //load x
-	#10000 Switches[8] = 0;
+	#60000 Switches[8] = 1; //load x  = 16
+	#10000 Switches = 10'b0000001000;
 	#10000 Switches[8] = 1; //load y
 	#10000 Switches[8] = 0; //Go!
 	#60000 Switches[8] = 1; //show y
