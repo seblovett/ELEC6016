bb6dd35b667a9560126a0dd37deb55b6130a4057
seblovett <seblovett@googlemail.com>
2014-04-01 20:25:51 +0100

    Added makefile

---
+++ Report/make.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# @file make.sh
+# Date Created: Tue 01 Apr 2014 19:46:45 BST by seblovett on seblovett-Ubuntu
+# <+Last Edited: Tue 01 Apr 2014 19:47:02 BST by seblovett on seblovett-Ubuntu +>
+
+pdflatex report
+pdflatex report
