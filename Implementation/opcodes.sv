1effda063df117b61ed0db0bee1c2e11ef80364f
seblovett <seblovett@googlemail.com>
2014-04-02 11:14:49 +0100

    Neated up the code

--- Implementation/opcodes.sv
+++ Implementation/opcodes.sv
@@ -7,18 +7,18 @@ typedef enum logic [1:0] {ALU_A = 2'd0, ALU_ADD = 2'd1, ALU_MULT = 2'd3, ALU_NOO
 typedef enum logic [1:0] {PcWait = 2'd2, PcInc = 2'd0, PcJmp = 2'd1} PcSel_t;
 typedef enum logic [3:0] {
 //	NOOP  = 4'd0,
-	WAIT0 = 4'd3,
-	WAIT1 = 4'd7,
 	STSW  = 4'd0,
-	//JMP   = 4'd5,
+	STACC = 4'd1,
+	WAIT0 = 4'd3,
 	JMPA  = 4'd5,
-//	JMPI  = 4'd7,
-	PASSA = 4'd12,
+	WAIT1 = 4'd7,
 	LUI   = 4'd8,
-	ADD   = 4'd13,
 	ADDI  = 4'd9,
-	MULT  = 4'd15,
-	STACC = 4'd1
+	PASSA = 4'd12,
+	ADD   = 4'd13,
+	MULT  = 4'd15
+	//JMP   = 4'd5,
+//	JMPI  = 4'd7,
 } opcodes_t;
 
 endpackage
