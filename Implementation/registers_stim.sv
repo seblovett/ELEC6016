7356924fd6e96ceed52cbba4e09de461735c27fc
seblovett <seblovett@googlemail.com>
2014-04-03 13:46:47 +0100

    Done testbench

--- Implementation/registers_stim.sv
+++ Implementation/registers_stim.sv
@@ -1,20 +1,21 @@
-// registers_tb.sv
+// registers_stim.sv
 // Writen by seblovett
 // Date Created Mon 17 Feb 2014 10:31:52 GMT
-// <+Last Edited: Mon 17 Feb 2014 11:33:56 GMT by hl13g10 on hind.ecs.soton.ac.uk +>
+// <+Last Edited: Thu 03 Apr 2014 13:45:50 BST by hl13g10 on hind.ecs.soton.ac.uk +>
 
 
-module registers_tb ();
+module registers_stim ();
 
 timeunit 1ns; timeprecision 1ps;
 
 parameter n = 8;
 parameter regcount = 32;
 parameter addr_width = 5;
-logic Clock, Reset, WE;  //control signals
-logic [n-1:0] Rd1, Rd2;
-logic [4:0] Rs1, Rs2, Rw;
-logic [n-1:0] Data;
+
+logic Clock, WE;  //control signals
+wire  [n-1:0] Rd1;
+logic [4:0] Rs1;
+logic [n-1:0] Data, temp;
 
 registers #(.n(n), .addr_width(addr_width), .regcount(regcount)) r (.*);
 
@@ -27,32 +28,47 @@ begin
         #250 Clock = 0;
 end
 
-//reset
-initial
-begin
-	Reset = 0;
-	#100 Reset = 1;
-	#100 Reset = 0;
-end
+int errors;
 
-//write some data
+task WriteandRead;
+	Data =$random();
+	temp = Data;
+	Rs1 = $random();
+	WE = 1;
+	@ (posedge Clock) //write clock edge
+	#20 WE = 0;
+	Data = $random(); //change the input
+	@ (posedge Clock) //need to wait a cycle to get the data back
+	#20 assert(Rd1 == temp) else begin errors++; $display("Write/Read Error"); end
+	@ (posedge Clock) //check that the data persists with new input, WE = 0;
+	#20 assert(Rd1 == temp) else begin errors++; $display("Persist Error"); end 
+endtask
+
+int i;
 initial 
 begin
-	Rs1 = 0; 
-	Rs2 = 0;
-	Data = 55;
-	Rw = 0;
+	i = 0;
+	errors = 0;
 	WE = 0;
-	#2000 WE = 1;
-	#1000 Rs2 = 30; //test outside the bounds
-	#3000 $stop();
+	temp = 0;
+	Rs1 = 0;
+	Data=0;
+	#1000
+	for ( i = 0; i < 100; i++)
+		WriteandRead();
+	
+	if(errors == 0)
+		$display("Simulation PASSED");
+	else
+		$display("Simulation FAILED with %d errors", errors);
+	$stop();
 end
 
 
 initial
 begin
-	$display("Time \tReset\tClock\tRs1=Rd1\tRs2=Rd2\tWE\tData");
-	$monitor("%0dns \t%d\t%d \t%d=%d\t%d=%d\t%d\t%d",$time,Reset, Clock,Rs1, Rd1, Rs2, Rd2, WE, Data);
+	$display("Time \tClock\tRs1\tRd1\tWE\tData");
+	$monitor("%0dns \t%d\t%d \t%d\t%d\t%d",$time,Clock,Rs1, Rd1, WE, Data);
 end
 endmodule
 
