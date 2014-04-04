7d68b8b773fa781474bd797434e65ab56c5ea34d
seblovett <seblovett@googlemail.com>
2014-04-04 13:34:31 +0100

    Added word counting.

--- Report/make.sh
+++ Report/make.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 # @file make.sh
 # Date Created: Tue 01 Apr 2014 19:46:45 BST by seblovett on seblovett-Ubuntu
-# <+Last Edited: Tue 01 Apr 2014 19:47:02 BST by seblovett on seblovett-Ubuntu +>
+# <+Last Edited: Fri 04 Apr 2014 10:34:52 BST by hl13g10 on octopus +>
 
 pdflatex report
 pdflatex report
+texcount -v -html -inc report.tex > count.html
